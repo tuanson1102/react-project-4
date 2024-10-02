5b03b306d6cd1a42e851dd22ee95211fc30b82d6
Tuan Son <tuanson0963345541@gmail.com>
2024-10-02 23:15:45 +0700

    add item

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@
 
 import "./App.css";
 import ProductCard from "./component/ProductCard";
-import ProductList from "./component/ProductList";
 import ProductCard2 from "./component/ProductCard2";
+import ProductList from "./component/ProductList";
 const products: Product[] = [
   {
     title: "Essence Mascara Lash Princess",
@@ -54,10 +54,9 @@ const products2: Product2[] = [
   {
     id: 1,
     image:
-      "https://cdn.mobilecity.vn/mobilecity-vn/images/2024/03/hinh-nen-hoa-tinh-yeu-mau-don.jpg.webp",
+      "https://cdn.pixabay.com/photo/2024/05/08/17/45/animal-8748794_1280.jpg",
     title: "His mother had always taught him",
     body: "His mother had always taught him not to ever think of himself as better than others. He'd tried to live by this motto. He never looked down on those who were less fortunate or who had less money than him. But the stupidity of the group of people he was talking to made him change his mind.",
-    // tags: ["history", "american", "crime"],
     reactions: {
       likes: 192,
       dislikes: 25,
@@ -68,10 +67,9 @@ const products2: Product2[] = [
   {
     id: 2,
     image:
-      "https://cdn.mobilecity.vn/mobilecity-vn/images/2024/03/hinh-nen-hoa-tinh-yeu-luu-ly.jpg.webp",
+      "https://media.istockphoto.com/id/1154370446/photo/funny-raccoon-in-green-sunglasses-showing-a-rock-gesture-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=kkZiaB9Q-GbY5gjf6WWURzEpLzNrpjZp_tn09GB21bI=",
     title: "He was an expert but not in a discipline",
     body: "He was an expert but not in a discipline that anyone could fully appreciate. He knew how to hold the cone just right so that the soft server ice-cream fell into it at the precise angle to form a perfect cone each and every time. It had taken years to perfect and he could now do it without even putting any thought behind it.",
-    // tags: ["french", "fiction", "english"],
     reactions: {
       likes: 859,
       dislikes: 32,
@@ -82,10 +80,9 @@ const products2: Product2[] = [
   {
     id: 3,
     image:
-      "https://cdn.mobilecity.vn/mobilecity-vn/images/2024/03/hinh-nen-hoa-tinh-yeu-tulip.jpg.webp",
+      "https://www.dartmoorzoo.org.uk/wp-content/uploads/2021/01/Tiger-1.jpg",
     title: "Dave watched as the forest burned up on the hill.",
     body: "Dave watched as the forest burned up on the hill, only a few miles from her house. The car had been hastily packed and Marta was inside trying to round up the last of the pets. Dave went through his mental list of the most important papers and documents that they couldn't leave behind. He scolded himself for not having prepared these better in advance and hoped that he had remembered everything that was needed.",
-    // tags: ["magical", "history", "french"],
     reactions: {
       likes: 1448,
       dislikes: 39,
@@ -96,10 +93,9 @@ const products2: Product2[] = [
   {
     id: 4,
     image:
-      "https://cdn.mobilecity.vn/mobilecity-vn/images/2024/03/hinh-nen-hoa-tinh-mau-tu-cam-chuong.jpg.webp",
+      "https://www.timeforkids.com/wp-content/uploads/2023/11/G3G5_231117_bear_steps.jpg?w=1024",
     title: "All he wanted was a candy bar.",
     body: "All he wanted was a candy bar. It didn't seem like a difficult request to comprehend, but the clerk remained frozen and didn't seem to want to honor the request. It might have had something to do with the gun pointed at his face.",
-    // tags: ["mystery", "english", "american"],
     reactions: {
       likes: 359,
       dislikes: 18,
@@ -110,10 +106,9 @@ const products2: Product2[] = [
   {
     id: 5,
     image:
-      "https://cdn.mobilecity.vn/mobilecity-vn/images/2024/03/hinh-nen-hoa-tinh-mau-tu-cam-tu-cau.jpg.webp",
+      "https://static.vecteezy.com/system/resources/thumbnails/023/701/963/small_2x/wild-leopard-animal-illustration-ai-generative-free-photo.jpg",
     title: "Hopes and dreams were dashed that day.",
     body: "Hopes and dreams were dashed that day. It should have been expected, but it still came as a shock. The warning signs had been ignored in favor of the possibility, however remote, that it could actually happen. That possibility had grown from hope to an undeniable belief it must be destiny. That was until it wasn't and the hopes and dreams came crashing down.",
-    // tags: ["crime", "mystery", "love"],
     reactions: {
       likes: 119,
       dislikes: 30,
@@ -124,10 +119,9 @@ const products2: Product2[] = [
   {
     id: 6,
     image:
-      "https://cdn.mobilecity.vn/mobilecity-vn/images/2024/03/hinh-nen-hoa-tinh-mau-tu-hoa-cuc.jpg.webp",
+      "https://d1jyxxz9imt9yb.cloudfront.net/animal/112/meta_image/regular/WR202206_GiraffeTranslocation_012_360559_reduced.jpg",
     title: "Dave wasn't exactly sure how he had ended up",
     body: "Dave wasn't exactly sure how he had ended up in this predicament. He ran through all the events that had lead to this current situation and it still didn't make sense. He wanted to spend some time to try and make sense of it all, but he had higher priorities at the moment. The first was how to get out of his current situation of being naked in a tree with snow falling all around and no way for him to get down.",
-    // tags: ["english", "classic", "american"],
     reactions: {
       likes: 15,
       dislikes: 8,
@@ -137,11 +131,9 @@ const products2: Product2[] = [
   },
   {
     id: 7,
-    image:
-      "https://cdn.mobilecity.vn/mobilecity-vn/images/2024/03/hinh-nen-hoa-tinh-mau-tu-hoa-chuong-xanh.jpg.webp",
+    image: "https://www.stockvault.net/data/2007/03/01/100489/thumb16.jpg",
     title: "This is important to remember.",
     body: "This is important to remember. Love isn't like pie. You don't need to divide it among all your friends and loved ones. No matter how much love you give, you can always give more. It doesn't run out, so don't try to hold back giving it as if it may one day run out. Give it freely and as much as you want.",
-    // tags: ["magical", "crime"],
     reactions: {
       likes: 127,
       dislikes: 26,
@@ -152,10 +144,9 @@ const products2: Product2[] = [
   {
     id: 8,
     image:
-      "https://cdn.mobilecity.vn/mobilecity-vn/images/2024/03/hinh-nen-hoa-tinh-mau-tu-hoa-dong-tien.jpg.webp",
+      "https://rspca.sfo2.cdn.digitaloceanspaces.com/public/Uploads/pig-animal-welfare-science__FocusFillWzYwNywzNjMsInkiLDEyMl0.jpg",
     title: "One can cook on and with an open fire.",
     body: "One can cook on and with an open fire. These are some of the ways to cook with fire outside. Cooking meat using a spit is a great way to evenly cook meat. In order to keep meat from burning, it's best to slowly rotate it.",
-    // tags: ["american", "english"],
     reactions: {
       likes: 1271,
       dislikes: 36,
@@ -166,10 +157,9 @@ const products2: Product2[] = [
   {
     id: 9,
     image:
-      "https://cdn.mobilecity.vn/mobilecity-vn/images/2024/03/hinh-nen-hoa-tinh-mau-tu-hoa-ho-diep.jpg.webp",
+      "https://img.freepik.com/free-photo/highly-detailed-view-fox-its-natural-environment_23-2151570814.jpg",
     title: "There are different types of secrets.",
     body: "There are different types of secrets. She had held onto plenty of them during her life, but this one was different. She found herself holding onto the worst type. It was the type of secret that could gnaw away at your insides if you didn't tell someone about it, but it could end up getting you killed if you did.",
-    // tags: ["american", "history", "magical"],
     reactions: {
       likes: 703,
       dislikes: 18,
@@ -180,10 +170,9 @@ const products2: Product2[] = [
   {
     id: 10,
     image:
-      "https://cdn.mobilecity.vn/mobilecity-vn/images/2024/03/hinh-nen-hoa-tinh-mau-tu-hoahong.jpg.webp",
+      "https://images.unsplash.com/photo-1552071379-041b32707fed?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8NXx8fGVufDB8fHx8fA%3D%3Dß",
     title: "They rushed out the door.",
     body: "They rushed out the door, grabbing anything and everything they could think of they might need. There was no time to double-check to make sure they weren't leaving something important behind. Everything was thrown into the car and they sped off. Thirty minutes later they were safe and that was when it dawned on them that they had forgotten the most important thing of all.",
-    // tags: ["fiction", "magical", "history"],
     reactions: {
       likes: 455,
       dislikes: 3,
